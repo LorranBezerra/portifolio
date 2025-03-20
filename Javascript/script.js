84168584f7edb30c71cb7d13051597f3e67b4651
lorran.bezerra <rodriguesbromen@gmail.com>
2025-03-20 10:28:14 -0300

    commit navbar concluida, iniciando home page, e section ja divididas

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,17 +1,21 @@
-const container = document.querySelector(".container");
-const primaryNav = document.querySelector(".nav__list");
-const toggleButton = document.querySelector(".nav-toggle");
+const menuIcon = document.querySelector(".menu-icon");
+const navLinks = document.querySelector(".nav-links");
+const navLinksInner = document.querySelectorAll(".nav-links li");
 
-toggleButton.addEventListener("click", () => {
-    const isExpanded = primaryNav.getAttribute("aria-expanded");
-    primaryNav.setAttribute(
-        "aria-expanded",
-        isExpanded === "false" ? "true" : "false"
-    );
-});
+menuIcon.addEventListener("click", () => {
+  // Alterna a classe 'menu-active' para mostrar/esconder o menu
+  navLinks.classList.toggle("menu-active");
 
-container.addEventListener("click", (e) => {
-    if (!primaryNav.contains(e.target) && !toggleButton.contains(e.target)) {
-        primaryNav.setAttribute("aria-expanded", "false");
+  // Anima os itens do menu
+  navLinksInner.forEach((li, index) => {
+    if (li.style.animation) {
+      li.style.animation = "";
+    } else {
+      li.style.animation = `navLinkAnime 0.5s ease forwards ${index / 7 + 0.3}s`;
     }
+  });
+
+  // Alterna a animação para o ícone do menu (transformando o ícone de hambúrguer em "X")
+  menuIcon.classList.toggle("span-anime");
 });
+   
